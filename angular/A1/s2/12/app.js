2e942708c1a443839fad3db8cbcaed6e1d1a3502
Dabiz <david.perez.rod@gmx.es>
2020-06-06 08:52:45 +0200

    A1-S2-11: var - let - const

---
+++ angular/A1/s2/12/app.js
@@ -0,0 +1,12 @@
+(function () {
+    var mensaje = 'Hola';
+    if (true) {
+        var mensaje_1 = 'mundo';
+    }
+    console.log(mensaje);
+    var nuevoMensaje = "nuevo hola";
+    if (true) {
+        var nuevoMensaje = "nuevo mundo";
+    }
+    console.log(nuevoMensaje);
+})();
---
+++ angular/A1/s2/12/app.ts
@@ -0,0 +1,15 @@
+(function (){
+    let mensaje = 'Hola';
+
+    if (true){
+        let mensaje= 'mundo';
+    }    
+    console.log(mensaje);
+
+    var nuevoMensaje= "nuevo hola";
+    if (true){
+        var nuevoMensaje="nuevo mundo";
+    }
+    console.log(nuevoMensaje); 
+})();
+
