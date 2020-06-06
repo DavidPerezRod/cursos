0f74e8a492188bd1707cd82ca17699ecb9170dc0
Dabiz <david.perez.rod@gmx.es>
2020-06-06 10:12:22 +0200

    A1-S2-13: variable types and declaration errors

---
+++ angular/A1/s2/13/withoutCompilationErrors.ts
@@ -0,0 +1,39 @@
+(function () {
+  //tsc error. 123 is not assignalbe to type 'string'
+  let variable1 = "Hola";
+  variable1 = "123";
+
+  //tsc error. "123" is not assignalbe to type number
+  let variable2 = 123;
+  variable2 = 123;
+
+  //It could be verbose and innecesary. It has sense if we don't assign any value in its inizialitation
+  let variable3: string = "cadena";
+  let variable4: number = 123;
+  let variable6: boolean= true;
+  let variable7: Date= new Date();
+
+  //different situation. Variable5 hasn't been initialize and it's type going to be "any". 
+  //But any, is a bad praxis. It admits any data type, and is a weak typing
+  let variable5;
+  variable5=variable3;
+  variable5=variable4;
+  variable5=variable6;
+  variable5=variable7;
+
+  //pipe for multiple types
+  let variable8: string | number;
+  variable8=variable3;
+  variable8=variable4;
+
+  //object types
+  let spiderman = {
+      name: "Peter",
+      age: 30
+  };
+
+  spiderman = {
+      name: "George",
+      age: 45
+  }
+})();
