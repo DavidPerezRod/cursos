22569231bac41442230a032470d71743b31557b1
David <david.perez.rod@gmx.es>
2020-10-20 11:34:27 +0200

    ending wiremock testint

--- java/testing/forks/tsbb2b-sfg-brewery/src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTest.java
+++ java/testing/forks/tsbb2b-sfg-brewery/src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTest.java
@@ -4,7 +4,6 @@ import guru.springframework.brewery.services.BeerService;
 import guru.springframework.brewery.web.model.BeerDto;
 import guru.springframework.brewery.web.model.BeerPagedList;
 import guru.springframework.brewery.web.model.BeerStyleEnum;
-import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.*;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
@@ -14,41 +13,37 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.core.Is.is;
-import static org.mockito.Mockito.reset;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.reset;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-//@ExtendWith(MockitoExtension.class)
 @WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
     @MockBean
     BeerService beerService;
 
-//    Con SpringBoot no hace falta inyectarlos, los inyecta directamente Spring boot al declarar la clase en la anotación @WebMvcTest
-//    @InjectMocks
-//    BeerController beerController;
-
-    @Autowired //este autowired hace falta con SpringBoot. No hace falta si se utiliza @ExtendsWith
+    @Autowired
     MockMvc mockMvc;
 
-    BeerDto beerDto;
+    BeerDto validBeer;
 
     @BeforeEach
-    void setUp(){
-        beerDto=BeerDto.builder().id(UUID.randomUUID())
+    void setUp() {
+        validBeer = BeerDto.builder().id(UUID.randomUUID())
                 .version(1)
                 .beerName("Beer1")
                 .beerStyle(BeerStyleEnum.PALE_ALE)
@@ -58,13 +53,10 @@ class BeerControllerTest {
                 .createdDate(OffsetDateTime.now())
                 .lastModifiedDate(OffsetDateTime.now())
                 .build();
-//    converter previo a la slecciones con Spring Boot. Con Spring Boot no es necesario configurar los mapper
-//        mockMvc = MockMvcBuilders.standaloneSetup(beerController).setMessageConverters(jackson2CborHttpMessageConverter()).build();
-
     }
 
     @AfterEach
-    void tearDown(){
+    void tearDown() {
         reset(beerService);
     }
 
@@ -72,33 +64,40 @@ class BeerControllerTest {
     void testGetBeerById() throws Exception {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ");
 
-        given(beerService.findBeerById(any())).willReturn(beerDto);
-        mockMvc.perform(get("/api/v1/beer/"+ beerDto.getId()))
+        given(beerService.findBeerById(any())).willReturn(validBeer);
+
+       MvcResult result=  mockMvc.perform(get("/api/v1/beer/" + validBeer.getId()))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(jsonPath("$.id", is(beerDto.getId().toString())))
+                .andExpect(jsonPath("$.id", is(validBeer.getId().toString())))
                 .andExpect(jsonPath("$.beerName", is("Beer1")))
-                .andExpect(jsonPath("$.createdDate", is(dateTimeFormatter.format(beerDto.getCreatedDate()))))
-        ;
+                .andExpect(jsonPath("$.createdDate",
+                        is(dateTimeFormatter.format(validBeer.getCreatedDate()))))
+               .andReturn();
+
+        System.out.println(result.getResponse().getContentAsString());
+
     }
 
     @DisplayName("List Ops - ")
     @Nested
-    public class TestListOperations{
+    public class TestListOperations {
+
         @Captor
         ArgumentCaptor<String> beerNameCaptor;
 
         @Captor
-        ArgumentCaptor<BeerStyleEnum> beerStyleEnumArgumentCaptor;
+        ArgumentCaptor<BeerStyleEnum> beerStyleEnumCaptor;
 
         @Captor
-        ArgumentCaptor<PageRequest> pageRequestArgumentCaptor;
+        ArgumentCaptor<PageRequest> pageRequestCaptor;
 
         BeerPagedList beerPagedList;
 
         @BeforeEach
-        void setUp(){
-            List<BeerDto> beers = Lists.newArrayList(beerDto);
+        void setUp() {
+            List<BeerDto> beers = new ArrayList<>();
+            beers.add(validBeer);
             beers.add(BeerDto.builder().id(UUID.randomUUID())
                     .version(1)
                     .beerName("Beer4")
@@ -111,29 +110,21 @@ class BeerControllerTest {
                     .build());
 
             beerPagedList = new BeerPagedList(beers, PageRequest.of(1, 1), 2L);
-            given(beerService.listBeers(beerNameCaptor.capture(), beerStyleEnumArgumentCaptor.capture(),
-                    pageRequestArgumentCaptor.capture())).willReturn(beerPagedList);
+
+            given(beerService.listBeers(beerNameCaptor.capture(), beerStyleEnumCaptor.capture(),
+                    pageRequestCaptor.capture())).willReturn(beerPagedList);
         }
 
-        @DisplayName("TEst List beers - no parameters")
+        @DisplayName("Test list beers - no parameters")
         @Test
         void testListBeers() throws Exception {
-            mockMvc.perform(get("/api/v1/beer").accept(MediaType.APPLICATION_JSON))
+            mockMvc.perform(get("/api/v1/beer")
+                        .accept(MediaType.APPLICATION_JSON))
                     .andExpect(status().isOk())
                     .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                     .andExpect(jsonPath("$.content", hasSize(2)))
-                    .andExpect(jsonPath("$.content[0].id", is(beerDto.getId().toString())));
+                    .andExpect(jsonPath("$.content[0].id", is(validBeer.getId().toString())));
         }
     }
 
-//    converter previo a la slecciones con Spring Boot. Con Spring Boot no es necesario configurar los mapper. Lo tenemos en la anotación WebMVc
-//    public MappingJackson2HttpMessageConverter jackson2CborHttpMessageConverter(){
-//        ObjectMapper objectMapper= new ObjectMapper();
-//        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-//        objectMapper.configure(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS, true);
-//        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-//
-//        objectMapper.registerModule(new JavaTimeModule());
-//        return new MappingJackson2HttpMessageConverter(objectMapper);
-//    }
 }
